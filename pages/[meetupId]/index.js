4cf6e6203f99084853748a9398b24616ac2f4f73
Sai <saikrishnadas666@gmail.com>
2021-05-18 12:43:57 +0530

    issue with newly added

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -26,7 +26,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: true,
+    fallback: false,
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
